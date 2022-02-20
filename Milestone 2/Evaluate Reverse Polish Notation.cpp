5d48e6887dd7f4f75a70927e64f7d476da6e1fb5
Surabhi <91132355+surabhi-paliwal@users.noreply.github.com>
2022-02-20 22:02:54 +0530

    Create Evaluate Reverse Polish Notation.cpp

---
+++ Milestone 2/Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,44 @@
+
+class Solution {
+public:
+    bool is_number(const std::string& s){
+        if(s.empty()) return false;
+        if(s.size() > 1 && s[0] == '-' && s.find_first_not_of("0123456789", 1) == std::string::npos) return true;
+        return s.find_first_not_of("0123456789") == std::string::npos;
+    };
+    
+    int evalRPN(vector<string>& tokens) {
+        stack<int> oprs;
+        
+        for(const string& token : tokens){
+            if(is_number(token)){
+                oprs.push(stoi(token));
+            }else{
+                int b = oprs.top(); oprs.pop();
+                int a = oprs.top(); oprs.pop();
+                
+                int res;
+                switch(token[0]){
+                    case '+':
+                        res = a+b;
+                        break;
+                    case '-':
+                        res = a-b;
+                        break;
+                    case '*':
+                        res = a*b;
+                        break;
+                    case '/':
+                        res = a/b;
+                        break;
+                }
+                
+                oprs.push(res);
+                
+                // cout << a << token << b << " = " << res << endl;
+            }
+        }
+        
+        return oprs.top();
+    }
+};
